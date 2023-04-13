f99a7002195630ffd8d49dca7c25493301daa1ce
youa2023 <129129552+youa2023@users.noreply.github.com>
2023-04-13 01:25:03 +0000

    mission1짝수,홀수 구분

--- if.c
+++ if.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-main() {
-  int a=5, b=3;
+// #include <stdio.h>
+// main() {
+//   int a=5, b=3;
 
-  if(5>3){
-    printf("A\n");
-  }
-  printf("B\n");
+//   if(5>3){
+//     printf("A\n");
+//   }
+//   printf("B\n");
   
-  if(5+3){ 
-    printf("C\n");
-  }
-  printf("D\n");
+//   if(5+3){ 
+//     printf("C\n");
+//   }
+//   printf("D\n");
 
-  if(5-5) {
-    printf("E\n");
-  }
+//   if(5-5) {
+//     printf("E\n");
+//   }
   
-  if(a>b) {
-    printf("F\n");
-  }
-  printf("G\n");
+//   if(a>b) {
+//     printf("F\n");
+//   }
+//   printf("G\n");
   
-  if(a<3){ 
-    printf("H\n");
-  }
+//   if(a<3){ 
+//     printf("H\n");
+//   }
   printf("I\n");
 }
